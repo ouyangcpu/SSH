4cbdc1a197092c7aba08ac5b09accd567f78f21b
ouyangcpu <814943616@qq.com>
2022-06-07 22:44:54 +0800

    Update 连续数组和.java

--- 连续数组和.java
+++ 连续数组和.java
@@ -6,32 +6,29 @@ public class MainTest {
     for (int i = 1; i < nums.length; i++) {
       nums[i] = i;
     }
-    int sum = 5;
-    int tempsum = nums[0];
+    int sum = 6;
     int[] result = new int[1];
-    if (tempsum == sum) {
-      result[0] = 1;
-      tempsum = 0;
-    }
-    for (int i = 1; i < nums.length; i++) {
-      huisu(i, nums, sum, tempsum, result);
+    for (int i = 0; i < nums.length; i++) {
+      huisu(i, nums, sum, result);
     }
     System.out.println(result[0]);
   }
 
-  // 1 2 3 5 6 7 8   5
-  private static void huisu(int i, int[] nums, int sum, int tempsum, int[] result) {
-    if (tempsum > sum) {
-      tempsum = 0;
+  // 0 1 2 3 5 6 7 8   5
+  private static void huisu(int i, int[] nums, int sum, int[] result) {
+    int tempsum = nums[i];
+    if (tempsum == sum) {
+      result[0] = Math.max(1, result[0]);;
+      return;
+    } else if (tempsum > sum) {
       return;
     }
-    for (int j = i; j < nums.length; j++) {
-      if (nums[j] - nums[j - 1] == 1) {
+    for (int j = i+1; j < nums.length; j++) {
+      if (nums[j] - nums[j-1] == 1) {
         tempsum = tempsum + nums[j];
         if (tempsum == sum) {
           result[0] = Math.max(j - i + 1, result[0]);
         } else if (tempsum > sum) {
-          tempsum = 0;
           return;
         }
       } else {
