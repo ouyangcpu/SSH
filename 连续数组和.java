df5a00e129e3976db4bcf3d4266a7600a3bd2f67
ouyangcpu <814943616@qq.com>
2022-06-07 22:02:58 +0800

    Add files via upload

---
+++ 连续数组和.java
@@ -0,0 +1,42 @@
+package ouyang.empty.demo;
+
+public class MainTest {
+  public static void main(String[] args) {
+    int[] nums = new int[10];
+    for (int i = 1; i < nums.length; i++) {
+      nums[i] = i;
+    }
+    int sum = 5;
+    int tempsum = nums[0];
+    int[] result = new int[1];
+    if (tempsum == sum) {
+      result[0] = 1;
+      tempsum = 0;
+    }
+    for (int i = 1; i < nums.length; i++) {
+      huisu(i, nums, sum, tempsum, result);
+    }
+    System.out.println(result[0]);
+  }
+
+  // 1 2 3 5 6 7 8   5
+  private static void huisu(int i, int[] nums, int sum, int tempsum, int[] result) {
+    if (tempsum > sum) {
+      tempsum = 0;
+      return;
+    }
+    for (int j = i; j < nums.length; j++) {
+      if (nums[j] - nums[j - 1] == 1) {
+        tempsum = tempsum + nums[j];
+        if (tempsum == sum) {
+          result[0] = Math.max(j - i + 1, result[0]);
+        } else if (tempsum > sum) {
+          tempsum = 0;
+          return;
+        }
+      } else {
+        return;
+      }
+    }
+  }
+}
